c1ee6c4e737a76f89df507c0394d6669f5d7f741
subhashreec1013-spec <subhashreec1013@gmail.com>
2026-01-19 13:56:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # SET YOUR API KEY HERE
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-flash-latest")
 
 @app.route("/")
 def home():
